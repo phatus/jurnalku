680889402b19ab45c4108f79f3e2689683f4f57b
agus widiyanto <trezeq@gmail.com>
2026-01-11 12:57:20 +0700

    Redirect home to admin login

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReportController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('filament.admin.auth.login');
 });
 
 Route::middleware(['auth'])->group(function () {
