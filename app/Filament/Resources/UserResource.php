24959777f4c994aee4ef082c3c7fd6aede7c897c
agus widiyanto <trezeq@gmail.com>
2026-01-13 21:52:59 +0700

    fix: add missing Builder import in UserResource

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -9,6 +9,8 @@ use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Auth;
 
 class UserResource extends Resource
 {
